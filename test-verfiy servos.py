a2c505c389fa8a18b2f2ecb64e43f92d6e9f1fb2
airpioa <127520901+airpioa@users.noreply.github.com>
2023-06-05 19:26:44 -0700

    Create test-verfiy servos.py

---
+++ test-verfiy servos.py
@@ -0,0 +1,11 @@
+
+from machine import PWM, Pin
+from time import sleep
+led = Pin(25, Pin.OUT)
+led.toggle()
+shoulder = PWM(Pin(15))
+               
+shoulder.freq(50)
+shoulder.duty_u16(1500)
+sleep(1)
+shoulder.duty_u16(5500)
